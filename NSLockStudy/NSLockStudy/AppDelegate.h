6ee6b276c414d28bf4e9671b426cb2a420e9c1e4
Beautilut <beatuilut@gmail.com>
2018-03-23 14:31:32 +0800

    创建

---
+++ NSLockStudy/NSLockStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSLockStudy
+//
+//  Created by Beautilut on 2018/3/23.
+//  Copyright © 2018年 Beautilut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSLockStudy/NSLockStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSLockStudy
+//
+//  Created by Beautilut on 2018/3/23.
+//  Copyright © 2018年 Beautilut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
